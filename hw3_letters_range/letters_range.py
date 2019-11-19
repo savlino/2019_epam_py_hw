d7d5c44404f9b425228b44cdfe44982cb54f4d58
savlino <23111649@mail.ru>
2019-11-19 11:45:19 +0300

    homework 2.1 letters_range-beta

---
+++ hw3_letters_range/letters_range.py
@@ -0,0 +1,29 @@
+''' function returns array of latin letters in alphabet order in given range
+, default values are: start='a', stop='z', step=1;
+function accepts: single parameter as stop key,
+two parameters as start and stop keys respectively,
+three parameters as start, stop and step key respectively,
+
+function also allows to accept a dictionary
+to replace default letters for tranliteration
+'''
+
+
+def letters_range(start='a', stop='z', step=1, **args):
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+    if stop == 'z':
+        stop, start = start, 'a'
+    start_index = letters.find(start.lower())
+    stop_index = letters.find(stop.lower())
+    if args:
+        for k in args:
+            letters = letters.replace(k, str(args[k]))
+    letters_arr = []
+    if step < 0:
+        for i in range(stop_index + 1, start_index + 1, -step):
+            letters_arr.append(letters[i])
+        letters_arr.reverse()
+        return letters_arr
+    for i in range(start_index, stop_index, step):
+        letters_arr.append(letters[i])
+    return letters_arr
