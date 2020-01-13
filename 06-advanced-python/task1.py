90c675d053a92f8b880411ac49c5e8f5d4fddcfa
savlino <23111649@mail.ru>
2020-01-13 17:13:27 +0300

    06-homework task1.py update

--- 06-advanced-python/task1.py
+++ 06-advanced-python/task1.py
@@ -15,20 +15,31 @@ class Graph:
 
     def __next__(self):
         if not self.unvisited:
+            print("")
+            self.nodes = []
+            self.unvisited = deque([v for v in E])
             raise StopIteration
-        curr = self.unvisited.popleft()
-        if curr not in self.nodes:
-            self.nodes.append(curr)
-        for t in self.E[curr]:
+        current = self.unvisited.popleft()
+        if current not in self.nodes:
+            self.nodes.append(current)
+            print(current, end=" ")
+        for t in self.E[current]:
             if t not in self.nodes:
+                print(t, end=" ")
                 self.nodes.append(t)
-        if not self.unvisited:
-            return self.nodes
         return self.__next__()
 
 
 E = {'A': ['B', 'C', 'D'], 'B': ['C'], 'C': [], 'D': ['A']}
+
 graph = Graph(E)
 
 for vertex in graph:
     print(vertex)
+
+# G = {'A': ['B', 'C', 'D'], 'B': ['C'], 'C': [], 'D': ['A', 'Z', 'X'], 'Z':[]}
+
+# graph2 = Graph(G)
+
+# for vertex in graph2:
+#     print(vertex)
