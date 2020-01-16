f41f1077fbdac762ab20975f4d8708ee98f7426f
savlino <23111649@mail.ru>
2020-01-16 16:01:07 +0300

    06-advanced-python task4 update

--- 06-advanced-python/task4.py
+++ 06-advanced-python/task4.py
@@ -1,9 +1,11 @@
 """
-graphic output of folder tree, works only for subfolders
+graphic output of folder tree, works only inside current working directory
 """
 
 import os
 import os.path
+import sys
+from contextlib import suppress
 
 root_depth = os.getcwd().count(os.sep)
 
@@ -21,26 +23,35 @@ class PrintableFolder:
         self.content = content
         self.abspath = path_finder(name, check_path=search_path)
 
-    def __str__(self, level=0):
+    def __repr__(self, level=0):
         print(f"{'|   ' * level}|-> V {self.name}")
         for root, dirs, files in os.walk(self.abspath):
             level = root.replace(self.name, '').count(os.sep) - root_depth
+            globals()[self.name] = []
             while dirs:
                 d = dirs[0]
-                PrintableFolder(d, search_path=root).__str__(level)
+                PrintableFolder(d, search_path=root).__repr__(level)
+                globals()[self.name].append(d)
+
                 dirs.remove(d)
             for f in files:
-                print(PrintableFile(f).__str__(level=level))
+                globals()[f] = None
+                globals()[self.name].append(globals()[f])
+                print(PrintableFile(f, level=level).__repr__())
         return ""
 
 
 class PrintableFile:
-    def __init__(self, name):
+    def __init__(self, name, level):
         self.name = name
 
-    def __str__(self, level):
-        return f"{'|   ' * level}|-> {self.name}"
+        self.level = level
+
+    def __repr__(self):
+        return f"{'|   ' * self.level}|-> {self.name}"
 
 
 folder1 = PrintableFolder('folder1')
 print(folder1)
+
+print(file3 in folder2)
